a57f7e60fbf669de02cc35d279c2346575e5ec26
Bohdan_Druzuik <bdruziuk@gmail.com>
2015-09-09 18:41:31 +0300

    Add shuffling of data array

--- src/mocks/_service-models/grid.model.js
+++ src/mocks/_service-models/grid.model.js
@@ -2,11 +2,21 @@ angular.module('app').service('gridModel', function () {
     // default model
     var data = [];
 
+    //generate random data array
     for(var i = 1; i < 1000; i++) {
         data.push({
             id: i,
-            value: i + "test"
+            value: i + " test"
         });
+    }
+
+    //shuffle data
+    for (var j = 0; j < data.length - 1; j++) {
+        var k = j + Math.floor(Math.random() * (data.length - j));
+        var temp = data[k];
+
+        data[k] = data[j];
+        data[j] = temp;
     }
 
     this.getData = function (start, count) {
